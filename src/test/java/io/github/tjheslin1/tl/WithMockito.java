7ceb0b5ebdb65bd449a33bc189e8114d32cc781a
Thomas Heslin <tjheslin1@gmail.com>
2016-10-01 23:08:03 +0100

    Removing uncalled methods from WithMockito Interface

--- src/test/java/io/github/tjheslin1/tl/WithMockito.java
+++ src/test/java/io/github/tjheslin1/tl/WithMockito.java
@@ -2,7 +2,6 @@ package io.github.tjheslin1.tl;
 
 import org.mockito.Mockito;
 import org.mockito.stubbing.OngoingStubbing;
-import org.mockito.verification.VerificationMode;
 
 public interface WithMockito {
 
@@ -12,29 +11,5 @@ public interface WithMockito {
 
     default <T> OngoingStubbing<T> when(T methodCall) {
         return Mockito.when(methodCall);
-    }
-
-    default <T> T verify(T mock) {
-        return Mockito.verify(mock);
-    }
-
-    default <T> T verify(T mock, VerificationMode mode) {
-        return Mockito.verify(mock, mode);
-    }
-
-    default VerificationMode times(int numOfInvocations) {
-        return Mockito.times(numOfInvocations);
-    }
-
-    default <T> T any() {
-        return Mockito.any();
-    }
-
-    default <T> T eq(T value) {
-        return Mockito.eq(value);
-    }
-
-    default <T> T spy(T object) {
-        return Mockito.spy(object);
     }
 }
